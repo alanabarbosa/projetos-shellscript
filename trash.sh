142a62f3dd4a63c33e13e226eccfb5bc1af5403c
alanabarbosa <itsalanayall@hotmail.com>
2023-12-26 12:42:20 -0300

    Add while trash

--- trash.sh
+++ trash.sh
@@ -3,31 +3,72 @@
 PACOTE="trash-cli"
 
 # Verifica se o pacote está instalado
-if dpkg -l | grep -q $PACOTE; then
-    # Verifica se a lixeira está vazia
-    if [ -z "$(trash-list)" ]; then
-        echo "A lixeira já está vazia"
-    else
-        # Se a lixeira não estiver vazia, esvazia
-        trash-empty
-        echo "Items excluídos com sucesso"
-    fi
-else
+if ! dpkg -l | grep -q $PACOTE ; then
     echo "$PACOTE não está instalado"
     read -p "Deseja instalar o pacote (y/n)?: " RESPOSTA
 
-    if [ "$RESPOSTA" = "y" ] || [ "$RESPOSTA" = "Y" ]; then
+    if [ ${RESPOSTA,,} = "y" ]; then
         sudo apt-get install $PACOTE
-        echo "Instalado com sucesso"
-        echo "Verificando a lixeira"
-        if trash-list &> /dev/null; then
-            echo "A lixeira já está vazia"
-        else
-            trash-empty
-            echo "Items excluídos com sucesso"
-        fi
-    else
+        echo
+        echo "Instalado com sucesso"        
+    elif [ ${RESPOSTA,,} = "n" ]; then
         echo "Encerrando o programa"
+        exit 1
+    else
+    	echo "Resposta inválida"
+    	exit 2
     fi
 fi
+
+
+while true
+do
+
+	echo
+	echo "Escolha uma opção do Menu:"
+	echo
+	echo "1 - Listar Arquivos lixeira"
+	echo "2 - Esvaziar lixeira"
+	echo "q - sair"
+	echo
+	read -p "Digite a opção: " OPCAO
+	echo
+	
+	TRASH_LIST=$(trash-list)
+
+
+	case "$OPCAO" in
+	   1)   
+	     if [ -z "$TRASH_LIST" ]; then
+		 echo "A lixeira está vazia. Não há nada para listar."
+		 break
+	     else
+		 echo "listando items lixeira..."
+		 echo
+		 trash-list
+		 echo "Items listados com sucesso."
+	     fi
+	     ;;
+	   2)    
+	     if [ -z "$TRASH_LIST" ]; then
+		 echo "A lixeira está vazia. Não há nada para esvaziar."
+		 break
+	     else
+		 echo "Esvaziando lixeira..."
+		 echo
+		 trash-empty
+		 echo "Lixeira esvaziada com sucesso."
+	     fi
+	     ;;
+	 q)
+	   echo "Saindo..."
+	   sleep 1
+	   break
+	   ;;
+	 *)
+	   echo "Opção Inválida"
+	   ;;
+	esac
+done
+
 
